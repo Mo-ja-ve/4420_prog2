cac79e993cbb0c4aedc1455d5eefabd7722d131e
Mojave <39601026+Mo-ja-ve@users.noreply.github.com>
2021-04-09 19:44:22 -0400

    Add files via upload

--- clientserver.c
+++ clientserver.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string>
+#include <cstring>
 #include <iostream>
 
 #define MSG_LENGTH 100
@@ -27,36 +28,58 @@ int main(){
      int fd[2];
      pipe(fd);
      int result;
+     msg_t msg;
 
 
-     if(fork() != 0){// client procsess
+     char c_send[6] = {'s','e','n','d',':','\0'};
+     string c_exit = "EXIT";
+     string c_string;
+     msg_type_t typ;
+     char client_msg[MSG_LENGTH];
+
+     cin >> c_string;
+     int n = c_string.length();
+     char temp_s[n+1];
+     strcpy(temp_s, c_string.c_str());
+     temp_s[5] = '\0';
+
+     int fork_id = fork();
+     if(fork_id == 0){// client procsess
+
           close(fd[READ]);
-          char c_send = "send:\0";
-          string c_exit = "EXIT";
-          char temp_s[6];
-          string c_string;
-          msg_type_t typ;
-          msg_t msg;
+          if(temp_s[0] == c_send[0] && temp_s[1] == c_send[1] && temp_s[2] == c_send[2] &&
+             temp_s[3] == c_send[3] && temp_s[4] == c_send[4] && temp_s[5] == c_send[5]){
 
-          cin >> c_string;
-          size_t length = c_string.copy(temp_s,0,5);
-          temp_s[length] ='\0';
-          if(temp_s == c_send){
+               int c_string_length = c_string.length();
                typ = REGULAR;
                msg.type = typ;
-               strcpy(msg.message_text, c_string);
-               cout<<endl<<"msg txt "<<msg.message_text;
+               c_string.erase(0,5);
+               strcpy(temp_s, c_string.c_str());
+               //temp_s = temp_s + 5;
+               strcpy(msg.message_text, temp_s);
+               //cout<<endl<<"msg txt "<<msg.message_text;
+               write(fd[WRITE], msg.message_text, n+1);
+               close(fd[WRITE]);
 
           }else{
                cout<<endl<<"not okay";
           }
 
 
-     }else{
-          close(fd[WRITE]);
-
+     }else{//  server procsess
+       close(fd[WRITE]);
+       while(1){
           //printf("server");
-     }
 
+          int input_read = -1;
+          input_read = read(fd[READ], msg.message_text, n+1);
+          //cout<<endl<<input_read;
+          if(input_read != -1){
+            int pidtemp = getpid();
+            cout<<endl<<"server:"<<pidtemp<<"  recieved "<<msg.message_text<<" from client:"<<fork_id;
+            //close(fd[READ]);
+          }
+      }
+     }
      return 0;
 }
